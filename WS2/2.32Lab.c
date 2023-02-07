17f86a84c10a236254036f2e55fe47710406a607
Harrazzack <89938949+Harrazzack@users.noreply.github.com>
2023-02-07 07:47:21 -0800

    Update 2.32Lab.c
    last name to header

--- WS2/2.32Lab.c
+++ WS2/2.32Lab.c
@@ -2,7 +2,7 @@
 * Description: a program with two inputs, current price and last month's price (both integers).
 *			   Then, output a summary listing the price, the change since last month, and the
 *			   estimated monthly mortgage computed as (currentPrice * 0.051) / 12.
-* Author names : Haroon and Vincent Nguyen
+* Author names : Haroon Razzack and Vincent Nguyen
 * Author emails : vincent.n.nguyen@sjsu.edu
 * Last modified date : 02 - 06 - 2023
 * Creation date : 02 - 06 - 2023
