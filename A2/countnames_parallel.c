06e21f767271018061566a5389c63c648255b8fd
Vincent <71682140+vnnguyen997@users.noreply.github.com>
2023-03-06 21:02:34 -0800

    Update countnames_parallel.c
    Added a pipe, but not reading the data correctly

--- A2/countnames_parallel.c
+++ A2/countnames_parallel.c
@@ -20,6 +20,13 @@ int main(int argc, char *argv[]) {
     int counts[MAX_NAMES] = {0};
     int num_names = 0;
 
+    // Create a pipe for communicating with child processes
+    int fd[2];
+    if (pipe(fd) == -1) {
+        fprintf(stderr, "Failed to create pipe.\n");
+        return 1;
+    }
+
     // Fork for each file and read names from the file
     for (int i = 1; i < argc; i++) {
         pid_t pid = fork();
@@ -28,8 +35,8 @@ int main(int argc, char *argv[]) {
             // Open the input file
             FILE *fp = fopen(argv[i], "r");
             if (fp == NULL) {
-                printf("Can't open file: %s\n", argv[i]);
-                return 1;
+                fprintf(stderr, "Can't open file: %s\n", argv[i]);
+                exit(1);
             }
 
             // Read names from the file and after each loop iteration of the while
@@ -41,33 +48,18 @@ int main(int argc, char *argv[]) {
                 if (line[strlen(line) - 1] == '\n') {
                     line[strlen(line) - 1] = '\0';
                 }
+                // Write the name to the pipe
+                write(fd[1], line, strlen(line) + 1);
                 // Checking for an empty line otherwise it compares new names to each
                 // of the names in the stored array, if a match is found, count is incremented
                 if (strlen(line) == 0) {
                     fprintf(stderr,"Warning - Line %d in %s is empty.\n", line_num, argv[i]);
-                } else {
-                    int found = 0;
-                    for (int j = 0; j < num_names; j++) {
-                        if (strcmp(line, lines[j]) == 0) {
-                            counts[j]++;
-                            found = 1;
-                            break;
-                        }
-                    }
-                    // if match is not found, then a new name has been found, the new name is added
-                    // to the lines array, the number of names is incremented, and the count for that
-                    // name is incremented to 1.
-                    if (!found) {
-                        strncpy(lines[num_names], line, sizeof(lines[num_names]));
-                        counts[num_names]++;
-                        num_names++;
-                    }
                 }
                 line_num++;
             }
             // Close the file
             fclose(fp);
-            // return 0;
+            exit(0);
         }
         else if (pid < 0) {
             // Error handling for failed fork
@@ -76,12 +68,34 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    close(fd[1]);
+
+
     // Wait for all child processes to complete
     for (int i = 1; i < argc; i++) {
         wait(NULL);
     }
 
-    // Prints the names and their counts
+    char line[MAX_NAMES_LENGTH];
+    while (read(fd[0], line, sizeof(line)) > 0) {
+        // Check if the name is already in the array
+        int found = 0;
+        for (int i = 0; i < num_names; i++) {
+            if (strcmp(line, lines[i]) == 0) {
+                counts[i]++;
+                found = 1;
+                break;
+            }
+        }
+        // Add the name to the array if it's not already there
+        if (!found) {
+            strncpy(lines[num_names], line, sizeof(lines[num_names]));
+            counts[num_names]++;
+            num_names++;
+        }
+    }
+
+    // Prints the unique names and their counts
     for (int i = 0; i < num_names; i++) {
         printf("%s: %d\n", lines[i], counts[i]);
     }
