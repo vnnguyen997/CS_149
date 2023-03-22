5b39edfcbcc407a381ce8e09a084be98b4d3e6ca
Vincent Nguyen <vnnguyen997@gmail.com>
2023-03-21 18:05:28 -0700

    made use of strtok to turn the commands and arguments into tokens to be read by execvp

--- A3/proc_manager.c
+++ A3/proc_manager.c
@@ -11,23 +11,52 @@ int main(void)
 {
     pid_t pid;
     int status;
+    int i = 0;
 
     // set up arrays for reading and holding commands
     char commands[MAX_COMMANDS][MAX_COMMANDS_LENGTH];
     char *args[MAX_COMMANDS_LENGTH];
 
+    // read commands from stdin
+    while (fgets(commands[i], sizeof(commands[i]), stdin) != NULL) {
 
-    if ((pid = fork()) < 0) {
-        printf("fork error");
-    } else if (pid == 0) {		/* child */
-        execvp(args[0], args);
-        printf("couldn't execute: %s", args[0]);
-        exit(127);
+        // create tokens from commands array separated by space or \n
+        char *token = strtok(commands[i], " \n");
+
+        // j keeps track of arguments in command
+        int j = 0;
+
+        // while token isn't null, save tokens to args array
+        while (token != NULL) {
+            args[j] = token;
+            j++;
+            //sets token pointer to null or to new token
+            token = strtok(NULL, " \n");
+        }
+
+        // set end of arguments to NULL for execvp
+        args[j] = NULL;
+
+        // increment commands pointer for next command
+        i++;
+
+
+        if ((pid = fork()) < 0) {
+            printf("fork error");
+        } else if (pid == 0) {        /* child */
+            execvp(args[0], args);
+            printf("couldn't execute: %s", args[0]);
+            exit(127);
+        }
+
+    }
+    /* parent */
+    while ((pid = waitpid(-1, &status, 0)) > 0) {
+        printf("DONE ");
+    }
+    if (pid == -1) {
+        printf("waitpid error");
     }
 
-    /* parent */
-    if ((pid = waitpid(pid, &status, 0)) < 0)
-        printf("waitpid error");
-    printf("DONE ");
     exit(0);
 }
