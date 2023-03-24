cd011df9d60ab1e84228800ca9088b63925c9f05
Vincent Nguyen <vnnguyen997@gmail.com>
2023-03-24 15:57:18 -0700

    each child creates output and error files

--- A3/proc_manager.c
+++ A3/proc_manager.c
@@ -1,3 +1,13 @@
+/**
+ * Description: Program that executes multiple commands at the same time using fork.
+ * Commands are read in through stdin, each command consists of the system call and the argument,
+ * each child process creates output files, error files, and executes the commands.
+ * Authors: Haroon Razzack and Vincent Nguyen
+ * Author emails: haroon.razzack@sjsu.edu and vincent.n.nguyen@sjsu.edu
+ * Last modified date: 03-24-2023
+ * Creation date: 03-21-2023
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,6 +24,9 @@ int main(void)
     int status;
     int i = 0;
     int command_count = 0;
+
+    // create output and error files for this child process
+    char out_file[20], err_file[20];
 
     // set up arrays for reading and holding commands
     char commands[MAX_COMMANDS][MAX_COMMANDS_LENGTH];
@@ -43,42 +56,54 @@ int main(void)
         i++;
         command_count++;
 
-        // create output and error files for this child process
-        char out_file[20], err_file[20];
-        sprintf(out_file, "%d.out", getpid());
-        sprintf(err_file, "%d.err", getpid());
-        int out_fd = open(out_file, O_RDWR | O_CREAT | O_APPEND, 0777);
-        int err_fd = open(err_file, O_RDWR | O_CREAT | O_APPEND, 0777);
-
-
         if ((pid = fork()) < 0) {
             printf("fork error");
         } else if (pid == 0) {        /* child */
+
+            // create files for each child
+            sprintf(out_file, "%d.out", getpid());
+            sprintf(err_file, "%d.err", getpid());
+            int out_fd = open(out_file, O_RDWR | O_CREAT | O_APPEND, 0777);
+            int err_fd = open(err_file, O_RDWR | O_CREAT | O_APPEND, 0777);
+
             // redirect stdout and stderr to output and error files
             dup2(out_fd, 1);
             dup2(err_fd, 2);
 
             // print start command
-            printf("Starting command %d: %s (child %d, parent %d)\n",
-                   command_count, args[0], getpid(), getppid());
+            printf("Starting command %d: child %d pid of parent %d)\n",
+                   command_count, getpid(), getppid());
             fflush(stdout);
 
-            //execute commands
+            //execute commands, assuming only 2 arguments will be taken
             execvp(args[0], args);
             printf("couldn't execute: %s", args[0]);
             exit(127);
+
+            close(out_fd);
+            close(err_fd);
         }
 
     }
     /* parent */
-    while ((pid = wait(&status)) > 0) {
+    while ((wait ( NULL) != -1) ) {
+        // create files for each child
+        sprintf(out_file, "%d.out", getpid());
+        sprintf(err_file, "%d.err", getpid());
+        int out_fd = open(out_file, O_RDWR | O_CREAT | O_APPEND, 0777);
+        int err_fd = open(err_file, O_RDWR | O_CREAT | O_APPEND, 0777);
+
+        printf("Finished child %d pid of parent %d", getpid(), getppid());
         if (WIFEXITED(status)) {
-            fprintf(stderr, "Child %d terminated normally with exit code: %d\n",
-                    pid, WEXITSTATUS(status));
+            // redirect stdout and stderr to output and error files
+            dup2(out_fd, 1);
+            fprintf(stderr, "Exited with exitcode = %d\n", WEXITSTATUS(status));
         } else if (WIFSIGNALED(status)) {
-            fprintf(stderr, "Child %d terminated abnormally with signal number: %d\n",
-                    pid, WTERMSIG(status));
+            dup2(err_fd, 2);
+            fprintf(stderr, "Killed with signal %d\n", WTERMSIG(status));
         }
+        close(out_fd);
+        close(err_fd);
     }
 
 
