544fabee18a00512e26d881ef80272301e8ee6e5
Vincent Nguyen <vnnguyen997@gmail.com>
2023-05-09 17:19:32 -0700

    filling in some of the todos

--- A6/template.c
+++ A6/template.c
@@ -1,77 +1,53 @@
-/*****************************************
- * Description: Program expands on Assignment 2 for CS149 which takes two files
- * with names in them and counts them in different threads. The counts will be kept
- * in a hashmap and printed at the end with the names.
- * Authors: Vincent Nguyen
- * Author emails: vincent.n.nguyen@sjsu.edu
- * Last modified date: 05-9-2023
- * Creation date: 05-04-2023
- *
- * CS149 SP23
- * Template for assignment 6
- * San Jose State University
- * originally prepared by Bill Andreopoulos
-*****************************************/
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <stdbool.h>
-
+/*****************************************
+//CS149 SP23
+//Template for assignment 6
+//San Jose State University
+//originally prepared by Bill Andreopoulos
+*****************************************/
 //thread mutex lock for access to the log index
-//you need to use this mutexlock for mutual exclusion
+//TODO you need to use this mutexlock for mutual exclusion
 //when you print log messages from each thread
 pthread_mutex_t tlock1 = PTHREAD_MUTEX_INITIALIZER;
-
 //thread mutex lock for critical sections of allocating THREADDATA
-//you need to use this mutexlock for mutual exclusion
+//TODO you need to use this mutexlock for mutual exclusion
 pthread_mutex_t tlock2 = PTHREAD_MUTEX_INITIALIZER;
-
 //thread mutex lock for access to the name counts data structure
-//you need to use this mutexlock for mutual exclusion
+//TODO you need to use this mutexlock for mutual exclusion
 pthread_mutex_t tlock3 = PTHREAD_MUTEX_INITIALIZER;
-
 void* thread_runner(void*);
 pthread_t tid1, tid2;
-
 //struct points to the thread that created the object.
 //This is useful for you to know which is thread1. Later thread1 will also
-//deallocate.
+deallocate.
 struct THREADDATA_STRUCT
 {
     pthread_t creator;
-
-    // added a filename for the file in the thread
-    char filename[30];
 };
 typedef struct THREADDATA_STRUCT THREADDATA;
-
 THREADDATA* p=NULL;
-
 //variable for indexing of messages by the logging function.
 int logindex=0;
 int *logip = &logindex;
-
 //The name counts.
 // You can use any data structure you like, here are 2 proposals: a linked list OR
-// an array (up to 100 names).
+an array (up to 100 names).
 //The linked list will be faster since you only need to lock one node, while for
-// the array you need to lock the whole array.
+the array you need to lock the whole array.
 //You can use a linked list template from A5. You should also consider using a hash
-// table, like in A5 (even faster).
+table, like in A5 (even faster).
 struct NAME_STRUCT
 {
     char name[30];
     int count;
 };
 typedef struct NAME_STRUCT THREAD_NAME;
-
 //array of 100 names
 THREAD_NAME names_counts[100];
-
 //node with name_info for a linked list
 struct NAME_NODE
 {
@@ -81,29 +57,17 @@ struct NAME_NODE
 /*********************************************************
 // function main
 *********************************************************/
-int main(int argc, char *argv[])
+int main()
 {
 //TODO similar interface as A2: give as command-line arguments three filenames of
-// numbers (the numbers in the files are newline-separated).
-    if (argc != 3) {
-        printf("Usage: program <file1> <file2>\n");
-        return 1;
-    }
-
-    THREADDATA td1, td2;
-    strcpy(td1.filename, argv[1]);
-    strcpy(td2.filename, argv[2]);
-
+    numbers (the numbers in the files are newline-separated).
     printf("create first thread");
-    pthread_create(&tid1,NULL,thread_runner,argv[1]);
-
+    pthread_create(&tid1,NULL,thread_runner,NULL);
     printf("create second thread");
-    pthread_create(&tid2,NULL,thread_runner,argv[2]);
-
+    pthread_create(&tid2,NULL,thread_runner,NULL);
     printf("wait for first thread to exit");
     pthread_join(tid1,NULL);
     printf("first thread exited");
-
     printf("wait for second thread to exit");
     pthread_join(tid2,NULL);
     printf("second thread exited");
@@ -118,16 +82,12 @@ void* thread_runner(void* x)
     pthread_t me;
     me = pthread_self();
     printf("This is thread %ld (p=%p)",me,p);
-
-
     pthread_mutex_lock(&tlock2); // critical section starts
     if (p==NULL) {
         p = (THREADDATA*) malloc(sizeof(THREADDATA));
         p->creator=me;
     }
     pthread_mutex_unlock(&tlock2); // critical section ends
-
-
     if (p!=NULL && p->creator==me) {
         printf("This is thread %ld and I created THREADDATA %p",me,p);
     } else {
@@ -137,7 +97,8 @@ void* thread_runner(void* x)
 * //TODO implement any thread name counting functionality you need.
 * Assign one file per thread. Hint: you can either pass each argv filename as a
 thread_runner argument from main.
-* Or use the logindex to index argv, since every thread will increment the logindex anyway
+* Or use the logindex to index argv, since every thread will increment the
+logindex anyway
 * when it opens a file to print a log message (e.g. logindex could also index
 argv)....
 * //Make sure to use any mutex locks appropriately
